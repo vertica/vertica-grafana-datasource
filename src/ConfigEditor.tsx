3a9de67fbe0b45948f77fe5ea1427d2ee48098d4
ajay-abrol2 <85120431+ajay-abrol2@users.noreply.github.com>
2025-09-01 16:25:40 +0530

    Update ConfigEditor.tsx

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -185,7 +185,7 @@ export class ConfigEditor extends PureComponent<Props, State> {
   };
 
   render() {
-    const { options, onOptionsChange } = this.props;
+    const { options } = this.props;
     const { jsonData, secureJsonFields } = options;
     const selectedTLSMode = jsonData.tlsmode
       ? SSL_MODE_OPTIONS.filter((mode) => mode.value === jsonData.tlsmode)[0]
@@ -383,17 +383,7 @@ export class ConfigEditor extends PureComponent<Props, State> {
             </p>
           </InfoBox>
         </div>
-         <DataSourceHttpSettings
-        defaultUrl="http://localhost:5433"
-        dataSourceConfig={options}
-        onChange={onOptionsChange}
-      />
-
-      {/* This renders the Secure Socks Proxy toggle */}
-      <SecureSocksProxySettings
-        options={options}
-        onOptionsChange={onOptionsChange}
-      />
+ 
    
 
       </>
